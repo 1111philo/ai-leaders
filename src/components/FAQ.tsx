64b8e949cdb3c25b138fdd44e6348662b993c0e5
Blake Bertuccelli-Booth <46652+bbertucc@users.noreply.github.com>
2026-03-17 13:43:42 -0500

    Append additional locations text to FAQ answer

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -51,7 +51,7 @@ const FAQ: React.FC = () => {
 
         {
             question: "Where do I take the course, and is there a place I can work from?",
-            answer: "The course is remote. Optional facility access is available at Louisiana Tech, University of Louisiana Lafayette, Tulane University, and University of Illinois Chicago."
+            answer: "The course is remote. Optional facility access is available at Louisiana Tech, University of Louisiana Lafayette, Tulane University, and University of Illinois Chicago. Additional locations are added as needed."
         },
         {
             question: "When is the next cohort?",
